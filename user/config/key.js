b404c673ec07fdb7c3dc731660aad92143a781c5
kevin.chen <don-pan.cwf@163.com>
2018-12-23 17:01:03 +0800

    添加登录接口；登录成功返回token；登录验证密码是否一致；

--- user/config/key.js
+++ user/config/key.js
@@ -1,9 +1,12 @@
 module.exports = {
     mongoURI:'mongodb://localhost:27017/users',
-    // mongoURI:"mongodb://test:bai123456@ds063150.mlab.com:63150/api"
+    // mongoURI:"mongodb://test:bai123456@ds063150.mlab.com:63150/api",
+    secretName:"secret"
 }
 
 /**
  * mongoURI 指向本地或服务器中的数据库,并将这个 key 以对象的形式 exports，方便其它模块中使用
  * 连接数据库一定要使用 mongodb，URL ,port 本地测试一定要添加，否则 保存不到数据库
+ * 
+ * secretKey 指向返回 token 加密名称
  */
